1c2e3e4d20d3bf3ceea7bb7dd035543fcb15c7cd
shreyaansjain <shreyaansjain60@gmail.com>
2021-09-22 21:56:52 +0530

    add api to get form data

--- Backend/index.js
+++ Backend/index.js
@@ -5,13 +5,15 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Handling **CORS**.
+app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.json({msg:"Hello from server"})
 })
 app.post("/submit",(req,res)=>{
-  console.log(res);
-  console.log("succesffull *****************")
+  console.log(req.body.name);
+  res.sendStatus(200)
 })
 
 app.listen(port, (req,res) =>{
