34c6e55a9e07c73c0bd3ceb8b14fee0df89f218d
shreyaansjain <shreyaansjain60@gmail.com>
2021-09-22 21:57:43 +0530

    add fetch in form

--- Frontend/src/Components/Form.js
+++ Frontend/src/Components/Form.js
@@ -5,7 +5,7 @@ const Form = () => {
   const [user,setUser]=useState({name: ""})
   let name,value;
   const handleInput=(e)=>{
-    console.log(e);
+    console.log(e.target.value);
     name= e.target.name;
     value= e.target.value;
     setUser({...user, [name]:value});
@@ -13,32 +13,28 @@ const Form = () => {
   const handleSubmit = async(e)=>{
     e.preventDefault();
     const {name}=user;
-    const res= await fetch("/submit",{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name
+    try{
+      const res= await fetch("/submit",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name
+        })
       })
-    })
-    const data = await res.json();
-  
-    console.log(data);
-    if(res.status===422|| !data){
-      console.log("invalid");
-      window.alert("invalid");
+      const data = await res.json();
+      console.log("succesfully registered");
     }
-    else{
-      console.log("valid");
-      window.alert("valid");
+    catch(err){
+      console.log(err);
     }
   }
 
   return (
     <form method="POST">
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" id="name" value={user.name} onChange={handleInput} />
+      <input type="text" name="name" id="name" value={user.valuevalue} onChange={handleInput} />
       <input type="submit" onClick={handleSubmit}/>
     </form>
   );
